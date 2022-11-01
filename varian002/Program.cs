2dc79ae4443b02d4be2c7dd76945cd19d36950b8
Kirill Kuznetsov <k.kirill1989@mail.ru>
2022-11-01 22:02:21 +0600

    максимальное и минимальное число

---
+++ varian002/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 6;
+int b = 14;
+
+int max = a; 
+
+if(b > max) max = b; 
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+int min = a;
+
+if(b < min) min = b;
+
+Console.Write("min = ");
+Console.WriteLine(min);
